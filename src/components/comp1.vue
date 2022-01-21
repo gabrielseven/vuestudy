f69f4eee4603edccf5625cb29a5a368c36493539
Gabriel Venancio <gabriel.venancio@vertti.com.br>
2022-01-21 09:35:07 -0300

    Aula 6

---
+++ src/components/comp1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>    
+        <h1>Olá Vue!</h1>
+        <p>Nome é {{ nome }} e estou no {{ clube }}</p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'comp1',
+        data(){
+            return {
+                nome: "Gabriel",
+                clube: "Charrete Esporte"
+            }
+
+        }    
+    }
+</script>
